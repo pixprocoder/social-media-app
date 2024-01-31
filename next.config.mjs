9560af3929cfabfa0964f4d9d61cb87522cb1f67
samsul kobir <makkobir79@gmail.com>
2024-01-31 12:36:30 +0200

    conflict resolved with masum

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {};
-=======
-const nextConfig = {
-  images: {
-    domains: ["www.gravatar.com"],
-  },
-};
->>>>>>> b5c033d50fb0df03bdd3b2aa6b742cc1b39df58d
 
 export default nextConfig;
