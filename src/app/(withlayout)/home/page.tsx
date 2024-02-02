f97aa9373bf11798979fa59a8589a65f1ff8e5fd
samsul kobir <makkobir79@gmail.com>
2024-02-02 12:12:22 +0200

    homepag responsive completed

--- src/app/(withlayout)/home/page.tsx
+++ src/app/(withlayout)/home/page.tsx
@@ -8,13 +8,19 @@ import RightSideBar from "../../../Components/home/RightSideBar";
 const AfterLoginHomePage = () => {
   return (
     <div>
-      {/* <section className="home_container ">
-      <div className="feed_container ">
-        <LeftSideBar />
-        <MainFeed />
-        <RightSideBar />
-      </div>
-    </section> */}
+      <section className="w-full lg:max-w-[1400px] mx-auto pt-4 ">
+        <div className="lg:grid lg:grid-cols-5 md:grid md:grid-cols-3 gap-4">
+          <div className="hidden lg:block lg:col-span-1 md:col-span-1 sticky top-0 h-[90vh]">
+            <LeftSideBar />
+          </div>
+          <div className="lg:col-span-3 md:col-span-1">
+            <MainFeed />
+          </div>
+          <div className="hidden lg:block lg:col-span-1 md:col-span-1 sticky top-0 h-[90vh]">
+            <RightSideBar />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
