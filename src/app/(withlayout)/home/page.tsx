a4e3dd4532c2958c58865caa2552d04e8d1e09bf
samsul kobir <makkobir79@gmail.com>
2024-02-05 22:16:38 +0200

    mobile responsive completed and showed the story like instagarm

--- src/app/(withlayout)/home/page.tsx
+++ src/app/(withlayout)/home/page.tsx
@@ -4,6 +4,7 @@ import "../../../Components/home/home.modules.css";
 import LeftSideBar from "../../../Components/home/LeftSideBar";
 import MainFeed from "../../../Components/home/MainFeed";
 import RightSideBar from "../../../Components/home/RightSideBar";
+import MobileStory from "@/Components/home/MobileStory";
 
 const AfterLoginHomePage = () => {
   return (
@@ -12,6 +13,18 @@ const AfterLoginHomePage = () => {
         <div className="lg:grid lg:grid-cols-5 md:grid md:grid-cols-3 gap-4">
           <div className="hidden lg:block lg:col-span-1 md:col-span-1 sticky top-0 h-[90vh]">
             <LeftSideBar />
+          </div>
+          <div className="block lg:hidden  ">
+            <div className="flex my-2 gap-3 px-2 overflow-x-auto ">
+              <MobileStory name="Story" me={true} />
+              <MobileStory name="Samad.." />
+              <MobileStory name="Kobir" />
+              <MobileStory name="Nuha.." />
+              <MobileStory name="Evana" />
+              <MobileStory name="John" />
+              <MobileStory name="John" />
+              <MobileStory name="John" />
+            </div>
           </div>
           <div className="lg:col-span-3 md:col-span-1">
             <MainFeed />
