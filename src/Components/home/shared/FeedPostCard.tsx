c7280e7086e48d1b3f8f3cd8ff60db42c6befc26
samsul kobir <makkobir79@gmail.com>
2024-02-07 23:29:38 +0200

    modal added

--- src/Components/home/shared/FeedPostCard.tsx
+++ src/Components/home/shared/FeedPostCard.tsx
@@ -8,6 +8,15 @@ import { FaSmile } from "react-icons/fa";
 import { useSubmitPostMutation } from "@/Redux/api/postApi";
 import { IPost } from "@/types/newsfeed";
 import { useAppSelector } from "@/Redux/hooks";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/Components/ui/dialog";
 
 const FeedPostCard = () => {
   const [postText, setPostText] = useState("");
@@ -54,12 +63,49 @@ const FeedPostCard = () => {
             placeholder="What's on your mind?"
           />
         </div>
-        <button
+        {/* <button
           type="submit"
           className="px-5 text-md py-2 font-bold text-white rounded bg-violet-500 hover:bg-violet-800 transition-colors duration-300"
         >
           Post
-        </button>
+        </button> */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <button
+              type="submit"
+              className="px-5 text-md py-2 font-bold text-white rounded bg-violet-500 hover:bg-violet-800 transition-colors duration-300"
+            >
+              Post
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <button type="submit">Save changes</button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </form>
     </div>
   );
