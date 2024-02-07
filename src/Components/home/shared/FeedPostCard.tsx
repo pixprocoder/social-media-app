3cfc1420f00c4b244f051fcd7e8967d461209a7d
samsul kobir <makkobir79@gmail.com>
2024-02-07 23:51:52 +0200

    shadcn avatar and button added

--- src/Components/home/shared/FeedPostCard.tsx
+++ src/Components/home/shared/FeedPostCard.tsx
@@ -1,13 +1,7 @@
 "use client";
 // Import necessary dependencies
-import React, { useState } from "react";
-import { BsThreeDots } from "react-icons/bs";
-import { Avatar, Card, Input } from "antd";
-import { MdPermMedia } from "react-icons/md";
-import { FaSmile } from "react-icons/fa";
-import { useSubmitPostMutation } from "@/Redux/api/postApi";
-import { IPost } from "@/types/newsfeed";
-import { useAppSelector } from "@/Redux/hooks";
+import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
+import { Button } from "@/Components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -17,6 +11,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/Components/ui/dialog";
+import { useSubmitPostMutation } from "@/Redux/api/postApi";
+import { useAppSelector } from "@/Redux/hooks";
+import { IPost } from "@/types/newsfeed";
+import { Input } from "antd";
+import React, { useState } from "react";
 
 const FeedPostCard = () => {
   const [postText, setPostText] = useState("");
@@ -51,61 +50,57 @@ const FeedPostCard = () => {
         onSubmit={handleSubmitPost}
       >
         <div className="flex w-full mr-2 items-center gap-2 justify-center">
-          <Avatar
-            className="w-full"
-            src="https://api.dicebear.com/7.x/miniavs/svg?seed=8"
-          />
-          <input
-            name="post"
-            className="w-full outline-none p-2 rounded-md"
-            value={postText}
-            onChange={(e) => setPostText(e.target.value)}
-            placeholder="What's on your mind?"
-          />
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Dialog>
+            <DialogTrigger asChild>
+              <input
+                name="post"
+                className="w-full outline-none p-2 rounded-md bg-white text-black"
+                value={postText}
+                onChange={(e) => setPostText(e.target.value)}
+                placeholder="What's on your mind?"
+              />
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="name"
+                    defaultValue="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="username"
+                    defaultValue="@peduarte"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <button type="submit">Save changes</button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
-        {/* <button
+        <Button
           type="submit"
+          disabled
           className="px-5 text-md py-2 font-bold text-white rounded bg-violet-500 hover:bg-violet-800 transition-colors duration-300"
         >
           Post
-        </button> */}
-        <Dialog>
-          <DialogTrigger asChild>
-            <button
-              type="submit"
-              className="px-5 text-md py-2 font-bold text-white rounded bg-violet-500 hover:bg-violet-800 transition-colors duration-300"
-            >
-              Post
-            </button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Input
-                  id="name"
-                  defaultValue="Pedro Duarte"
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Input
-                  id="username"
-                  defaultValue="@peduarte"
-                  className="col-span-3"
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <button type="submit">Save changes</button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        </Button>
       </form>
     </div>
   );
